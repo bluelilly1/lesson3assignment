49d181a5a633a7d5ffa9420010c3eb0659c51aa4
Bliss Singhal <54075580+bluelilly1@users.noreply.github.com>
2021-07-15 16:06:29 -0700

    Create lesson3.java

---
+++ lesson3.java
@@ -0,0 +1,58 @@
+
+class Main {
+  //Your Name: 
+  public static void main(String[] args) {
+    //Assignment: 
+    //If statements
+    //Create two integers and assign them to any integer or expression you want
+
+    //if they are equal, print out the integers and the phrase "They are equal!"
+
+    //if they are not equal, print out which one is greater
+
+    //While Loops
+    //make another integer variable called hi
+    //print out the phrase "hello" for hi number of times
+
+    //For Loops 
+    //do the same thing using a for loop
+
+    //CHALLENGE
+    //What happens if you make a for loop inside a for loop??
+     
+
+    //References
+    //if statement Example
+    int int1 = 3*3+1;
+    int int2 = 4*2;
+    if (int1 == int2) {
+      System.out.println("These integers are the same");
+    }
+    else {
+      System.out.println("These integers aren't the same");
+    }
+   
+    //While loop Example
+    while (int2 < int1) {
+      System.out.println("Int2 is still less than int1");
+      int2++; //Adds one to int2
+    }
+    //for loop example
+    int myAge = 14;
+    for (int count = 0; count < myAge; count++) {
+      System.out.println(count + " is less than my age");
+    }
+
+
+
+
+
+
+
+  
+
+
+
+
+  }
+}
